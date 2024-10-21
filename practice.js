9bb707bd54570483364df40ce40065a0969c358a
HudsonPWesel <cswyld7k@gmail.com>
2024-10-21 18:11:38 -0400

    Fake Practice

---
+++ practice.js
@@ -0,0 +1,37 @@
+// const Person = {
+// 	name: 'JOhn',
+// 	age: 239,
+// 	greet() {
+// 		return this.name;
+// 	},
+// };
+
+// items = ['23232', 23232];
+// for (const item of items) {
+// 	console.log(item)
+// }
+// items = [1, 2, 3, 4, 5];
+// console.log(['new', ...items]);
+// const [a, ...rest] = items;
+
+// const printName = ({ name }) => {
+// 	console.log('Hello', name);
+// };
+// person = { name: 'Hudson' };
+// printName(person);
+
+// # ASYNC CODE (non-blocking code)
+
+// const prom = new Promise((resolve, reject) => {})
+const promise = new Promise((resolve, reject) => {});
+// const fetchData = callback => {
+// 	setTimeout(() => {
+// 		callback('FINALLY Done');
+// 	}, 1500);
+// };
+// setTimeout(() => {
+// 	console.log('Timer DOne');
+// 	fetchData(text => {
+// 		console.log(text);
+// 	});
+// }, 2000);
