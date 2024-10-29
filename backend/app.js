2a3b8d4f73123c8398e03bc7324c4a393293227d
HudsonPWesel <cswyld7k@gmail.com>
2024-10-29 08:32:46 -0400

    Parse requests

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,21 @@
 const http = require('http');
-// IMported
-function reqestListener(res, res) {}
-const server = http.createServer((req, res) => {
-	console.log(req);
-});
 
+const server = http.createServer((req, res) => {
+	res.write('<html>');
+	res.write('<head><title>Enter Message</title><head>');
+	res.write(
+		'<body><form action="/message" method="post"><input type="text" name="message"></input></form></body>'
+	);
+	res.write('<html>');
+	const data = [];
+	req.on('data', chunk => {
+		console.log(chunk);
+		data.push(chunk);
+	});
+	req.on('end', () => {
+		const parsedBody = Buffer.concat(data).toString();
+		console.log(parsedBody);
+	});
+	res.end();
+});
 server.listen(3000);
