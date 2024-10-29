88b2a34fec9c6597c1d482a3b26d42cc8c216009
HudsonPWesel <cswyld7k@gmail.com>
2024-10-29 09:07:56 -0400

    Using send and app.listen

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,12 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
 	console.log('Hello from another middlware');
+
+	// Sets properties of request for us and ensures we don't have a hanging request
+	res.send('Hello from express!');
 });
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
+// server.listen(3000);
 
-server.listen(3000);
+app.listen(3000);
